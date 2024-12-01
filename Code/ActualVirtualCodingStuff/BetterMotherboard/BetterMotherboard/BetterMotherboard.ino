454aa8a4690e0e746c1088d45dec4316bf5f329b
Jstn1321 <iamaalt921@gmail.com>
2024-11-30 18:07:30 -0700

    FINALLY FIXED ARDUINO CODE SO STEPPER MOTORS NOW RUN AT NORMAL SPEEDS

--- Code/ActualVirtualCodingStuff/BetterMotherboard/BetterMotherboard/BetterMotherboard.ino
+++ Code/ActualVirtualCodingStuff/BetterMotherboard/BetterMotherboard/BetterMotherboard.ino
@@ -1,8 +1,20 @@
 #include <ezButton.h>
 #include <AccelStepper.h>
+#include <util/delay.h>
+
+#define J1step 41
+#define J1dir 42
+#define J2step 43
+#define J2dir 44
+#define J3step 45
+#define J3dir 46
 
 int joy1x;
 int joy1y;
+int joy2x;
+int joy2y;
+int joy3x;
+int joy3y;
 
 //AccelStepper stepper6(1, 51, 52); // (Type of driver: with 2 pins, STEP, DIR)
 //AccelStepper stepper5(1, 49, 50); // (Type of driver: with 2 pins, STEP, DIR)
@@ -27,25 +39,68 @@ void setup() {
 }
 
 void loop() {
+  joy1x = analogRead(A0);
+  joy1y = analogRead(A1);
+  joy2x = analogRead(A2);
   // put your main code here, to run repeatedly:
-  joy1x = (analogRead(A0));
-  joy1y = (analogRead(A1));
-
-  //joy1y = map(analogRead(A1), 0, 1023, -1000, 1000);
   if (joy1x > 700){
-    stepper2.setSpeed(5000); // Forward
-    stepper2.runSpeed();
+    digitalWrite(J1dir, HIGH);
+    while (joy1x > 700){
+      digitalWrite(J1step,HIGH); 
+      __asm__("nop\n\t");
+      digitalWrite(J1step,LOW); 
+      __asm__("nop\n\t");
+      joy1x = analogRead(A0);
+    }
   } 
   else if (joy1x < 200) {
-    stepper2.setSpeed(-5000); // Reverse
-    stepper2.runSpeed();
+    digitalWrite(J1dir, LOW);
+    while (joy1x < 200){
+      digitalWrite(J1step,HIGH); 
+      __asm__("nop\n\t");    // by changing this time delay between the steps we can change the rotation speed
+      digitalWrite(J1step,LOW); 
+      __asm__("nop\n\t");
+      joy1x = analogRead(A0);
+    }
   }
   if (joy1y > 700){
-    stepper1.setSpeed(10000);
-    stepper1.runSpeed();
+    digitalWrite(J2dir, HIGH);
+    while (joy1y > 700){
+      digitalWrite(J2step,HIGH); 
+      delayMicroseconds(450);    // by changing this time delay between the steps we can change the rotation speed
+      digitalWrite(J2step,LOW); 
+      delayMicroseconds(450); 
+      joy1y = analogRead(A1);
+    }
   }
   else if (joy1y < 200){
-    stepper1.setSpeed(-10000);
-    stepper1.runSpeed();
+    digitalWrite(J2dir, LOW);
+    while (joy1y < 200){
+      digitalWrite(J2step,HIGH); 
+      delayMicroseconds(450);    // by changing this time delay between the steps we can change the rotation speed
+      digitalWrite(J2step,LOW); 
+      delayMicroseconds(450); 
+      joy1y = analogRead(A1);
   }
+}
+  if (joy2x > 700){
+    digitalWrite(J3dir, HIGH);
+    while (joy2x > 700){
+      digitalWrite(J3step,HIGH); 
+      delayMicroseconds(200);    // by changing this time delay between the steps we can change the rotation speed
+      digitalWrite(J3step,LOW); 
+      delayMicroseconds(200); 
+      joy2x = analogRead(A2);
+    }
+  }
+  else if (joy2x < 200){
+    digitalWrite(J3dir, LOW);
+    while (joy2x < 200){
+      digitalWrite(J3step,HIGH); 
+      delayMicroseconds(200);     // by changing this time delay between the steps we can change the rotation speed
+      digitalWrite(J3step,LOW); 
+      delayMicroseconds(200); 
+      joy2x = analogRead(A2);
+  }
+}
 }
