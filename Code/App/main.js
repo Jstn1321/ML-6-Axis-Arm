fa739d7e6c5ddaa37cfe54548b1d894726428d2b
Justin Nguyen <iamaalt921@gmail.com>
2024-12-31 13:35:21 -0700

    Now when you press button it runs the python script and prints its output in the console

--- Code/App/main.js
+++ Code/App/main.js
@@ -1,4 +1,6 @@
-const { app, BrowserWindow } = require('electron/main');
+const { app, BrowserWindow, ipcMain } = require('electron');
+const { exec } = require('child_process');
+const path = require('path');
 
 const createWindow = () => {
   const win = new BrowserWindow({
@@ -12,6 +14,44 @@ const createWindow = () => {
 
   win.loadFile('index.html');
 };
+
+ipcMain.on('takePic', (event) => {
+  exec('cmd.exe /c "conda activate tf && python ./scripts/takePic.py"', (err, stdout, stderr) => {
+    if (err) {
+      console.error(`exec error: ${err}`);
+      event.reply('console-message', `Error: ${stderr}`);
+      return;
+    }
+    console.log(`stdout: ${stdout}`);
+    event.reply('console-message', `${stdout}`);
+    
+    event.sender.send('update', path.join(__dirname, 'scripts/assets/takenPic/filename.jpg'));
+  });
+});
+
+ipcMain.on('identify', (event) => {
+  exec('cmd.exe /c "conda activate tf && python ./scripts/identify.py"', (err, stdout, stderr) => {
+    if (err) {
+      console.error(`exec error: ${err}`);
+      event.reply('console-message', `Error: ${stderr}`);
+      return;
+    }
+    console.log(`stdout: ${stdout}`);
+    event.reply('console-message', `${stdout}`);
+  });
+});
+
+ipcMain.on('sort', (event) => {
+  exec('cmd.exe /c "conda activate tf && python ./scripts/sort.py"', (err, stdout, stderr) => {
+    if (err) {
+      console.error(`exec error: ${err}`);
+      event.reply('console-message', `Error: ${stderr}`);
+      return;
+    }
+    console.log(`stdout: ${stdout}`);
+    event.reply('console-message', `${stdout}`);
+  });
+});
 
 app.whenReady().then(() => {
   createWindow();
