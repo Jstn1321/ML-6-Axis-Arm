5a65f9e654317458ba0fa3ccd2f014dbbb56e1dd
Justin Nguyen <iamaalt921@gmail.com>
2024-12-30 21:02:41 -0700

    Moved to a more appropriate spot

---
+++ Code/App/scripts/sendInvKine.py
@@ -0,0 +1,23 @@
+import serial
+import time
+import invKine
+import numpy
+
+# Configure the serial connection
+arduino = serial.Serial(port='COM5', baudrate=115200, timeout=1)
+time.sleep(2)  # Allow time for the connection to initialize
+
+# Generate joint angles
+jointAngles = invKine.ik([0.52, 0.1, 0.3])
+
+# Prepare data for sending
+data_string = ','.join(map(str, jointAngles)) + '\n'  # Add newline for Arduino to read
+
+print(invKine.ik([0.52, 0.1, 0.3]))
+# Wait for a response
+while True:
+    arduino.write(data_string.encode())  # Send to Arduino
+    response = arduino.readline().decode().strip()
+    if response:
+        jointAngles = list(map(float, response.split(',')))
+        print(jointAngles)
