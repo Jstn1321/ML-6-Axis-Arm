12e70745de419de5c9b6e4d0afaebe5de0585ba4
Justin Nguyen <iamaalt921@gmail.com>
2025-01-02 22:24:13 -0700

    Added script to tell arduino its on the auto mode

---
+++ Code/App/scripts/setAuto.py
@@ -0,0 +1,11 @@
+import serial
+import time
+import invKine
+import numpy
+
+arduino = serial.Serial(port='COM5', baudrate=115200, timeout=1)
+time.sleep(2) 
+
+message = "a"
+
+arduino.write(message.encode()) 
