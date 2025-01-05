07882e76bb3ae2dc148549c5392fa93bfa3a24fc
Justin Nguyen <iamaalt921@gmail.com>
2025-01-05 16:51:27 -0700

    Fixed window height

--- Code/App/main.js
+++ Code/App/main.js
@@ -5,7 +5,7 @@ const path = require('path');
 const createWindow = () => {
   const win = new BrowserWindow({
     width: 800,
-    height: 750,
+    height: 760,
     webPreferences: { 
       nodeIntegration: true,
       contextIsolation: false,
