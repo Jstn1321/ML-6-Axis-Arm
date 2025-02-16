d8db066ee0ea1901ec054b1f1824ee58002b3c08
Justin Nguyen <iamaalt921@gmail.com>
2025-02-15 17:46:16 -0700

    Checks if there is a blue/green box to prevent error

--- Code/App/scripts/sortYOLO.py
+++ Code/App/scripts/sortYOLO.py
@@ -2,6 +2,7 @@ import json
 import os
 import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
+import sendInvKine
 
 json_path = os.path.join(os.path.dirname(__file__), 'assets/boxCoords.json')
 with open(json_path, "r") as file:
@@ -9,19 +10,19 @@ with open(json_path, "r") as file:
 
 blueCoord = 0
 greenCoord = 0
-bluebox = data["bluebox"]
-greenbox = data["greenbox"]
+#bluebox = data["bluebox"]
+#greenbox = data["greenbox"]
 bluex = 0
 bluey = 0
 greenx = 0
 greeny = 0
 
 #in x y x y
-if bluebox == True:
+if data.get("bluebox"):
     blueCoord = data["blueCoord"]
     bluex = ((blueCoord[2] - blueCoord[0])/2) + blueCoord[0]
     bluey = ((blueCoord[3] - blueCoord[1])/2) + blueCoord[1]
-if greenbox == True:
+if data.get("greenbox"):
     greenCoord = data["greenCoord"]
     greenx = ((greenCoord[2] - greenCoord[0])/2) + greenCoord[0]
     greeny = ((greenCoord[3] - greenCoord[1])/2) + greenCoord[1]
@@ -29,12 +30,17 @@ if greenbox == True:
 print(blueCoord)
 print(greenCoord)
 
+sendInvKine.sendInvKineToArd([0.4,0.0,0.2])
+"""
 plt.title("Box Image")
 plt.xlabel("X pixel scaling")
 plt.ylabel("Y pixels scaling")
-plt.plot(bluex, bluey, marker='v', color="purple")
-plt.plot(greenx, greeny, marker='v', color="yellow")
+if data.get("bluebox"):
+    plt.plot(bluex, bluey, marker='v', color="purple")
+if data.get("greenbox"):
+    plt.plot(greenx, greeny, marker='v', color="yellow")
 
 image = mpimg.imread(os.path.join(os.path.dirname(__file__), "assets/takenPic/filename.jpg"))
 plt.imshow(image)
 plt.show()
+"""
